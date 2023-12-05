6e636d2a7d44666848e0a54acdee512ccaf2bfc1
ion <ion@n0de.biz>
2023-12-05 18:27:19 +0000

    upd

--- test-vm.sh
+++ test-vm.sh
@@ -4,6 +4,6 @@ HOST=${1:?Please set host}
 
 nix build .#nixosConfigurations.$HOST.config.system.build.vm
 
-QEMU_NET_OPTS="hostfwd=tcp::2222-:22" ./result/bin/run-xeus-vm
+QEMU_NET_OPTS="hostfwd=tcp::2222-:22" ./result/bin/run-xeus-vm &
 
 echo "connect with: ssh -p 2222 ion@127.0.0.1"
