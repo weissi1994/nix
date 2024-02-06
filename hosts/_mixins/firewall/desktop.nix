456d6e9a781ea85c8c80b99b236888451e4eee06
ion <ion@n0de.biz>
2024-02-06 21:06:10 +0000

    upd

--- hosts/_mixins/firewall/desktop.nix
+++ hosts/_mixins/firewall/desktop.nix
@@ -1,4 +1,3 @@
-{ lib, pkgs, ... }:
 {
   services.opensnitch = {
     rules = {
@@ -12,6 +11,54 @@
           "sensitive" = false;
           "operand" = "dest.port";
           "data" = "53";
+        };
+      };
+      chrome-notifications = {
+        "name" = "allow-chrome-notifications";
+        "enabled" = true;
+        "action" = "allow";
+        "duration" = "always";
+        "operator" = {
+          "type" = "simple";
+          "sensitive" = false;
+          "operand" = "dest.port";
+          "data" = "5228";
+        };
+      };
+      chrome-quic = {
+        "name" = "allow-chrome-quic";
+        "enabled" = true;
+        "action" = "allow";
+        "duration" = "always";
+        "operator" = {
+          "type" = "simple";
+          "sensitive" = false;
+          "operand" = "dest.port";
+          "data" = "1900";
+        };
+      };
+      spotify = {
+        "name" = "allow-spotify";
+        "enabled" = true;
+        "action" = "allow";
+        "duration" = "always";
+        "operator" = {
+          "type" = "simple";
+          "sensitive" = false;
+          "operand" = "dest.port";
+          "data" = "57621";
+        };
+      };
+      http = {
+        "name" = "allow-http";
+        "enabled" = true;
+        "action" = "allow";
+        "duration" = "always";
+        "operator" = {
+          "type" = "simple";
+          "sensitive" = false;
+          "operand" = "dest.port";
+          "data" = "80";
         };
       };
       https = {
